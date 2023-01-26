520d3dc5eda31f8378c3ed1a853ee63dcfcb7ff8
Speelgon <113151829+Speelgon@users.noreply.github.com>
2023-01-26 13:43:36 +0800

    Merged with yuki's branch

--- Alpha Engine 1/Alpha Engine 1/Incrementvariable.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-bool passedoverobj();
-
-int whichvariableincreased(int incrementobjintializer,int& a, int& b, float middlex, float middley, float optionhalfside, AEGfxVertexList* pMesh, AEGfxVertexList* pMesh2, int mousex, int mousey);
-
