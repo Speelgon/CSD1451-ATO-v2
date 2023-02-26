5b8f45cc5d13e694ad01c182d75e767c4e24e100
Speelgon <113151829+Speelgon@users.noreply.github.com>
2023-02-26 15:39:13 +0800

    updated catdeath to take framerate

--- Alpha Engine 1/Alpha Engine 1/catdeath.cpp
+++ Alpha Engine 1/Alpha Engine 1/catdeath.cpp
@@ -2,17 +2,17 @@
 #include "objects.hpp"
 
 extern f64 delta;
-
+extern f64 assumedFrameRate;
 
 void catdeath() {
 	if ((timeDead += delta) < deathSeconds)
 	{
 		player.yvel += 0.3;
-		player.y += player.yvel;
+		player.y += player.yvel * delta * assumedFrameRate;
 	}
 	else 
 	{
 		player.yvel -= 2*gravity;
-		player.y += player.yvel;
+		player.y += player.yvel * delta * assumedFrameRate;
 	}
 }
