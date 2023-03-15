1e42d9219fecee16be2f4c38dd254e8b317a093c
Speelgon <113151829+Speelgon@users.noreply.github.com>
2023-03-15 22:43:00 +0800

    Limited player distance from hook

--- Alpha Engine 1/Alpha Engine 1/HookUpdate.cpp
+++ Alpha Engine 1/Alpha Engine 1/HookUpdate.cpp
@@ -11,12 +11,20 @@ void hookUpdate() {
 	if (hookCollisionFlag == 1)	{
 		playerHook.x = pointHookStuckX;
 		playerHook.y = pointHookStuckY;
+
+		while (player.x - playerHook.x > item.height * 2) {
+			player.x -= 0.5;
+		}
+		while (player.x - playerHook.x < -item.height * 2) {
+			player.x += 0.5;
+		}
+		while (player.y - playerHook.y > item.height * 2) {
+			player.y -= 0.5;
+		}
+		while (player.y - playerHook.y < -item.height * 2) {
+			player.y += 0.5;
+		}
 	}
-}
 
-
-
-void itemUpdate() {
-	
 }
 
